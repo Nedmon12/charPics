599f6c328b01bffade68205e8b5fdf4a14bd5b06
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-02-25 01:44:58 +0300

    silly realization prevents redesign

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 from PIL import Image
-
-image = Image.open("./lilly.png")
-
-image = image.convert("L")
+import numpy as np
 
 
-width, height = image.size
-string_representation = ""
+def stringrep(filename):
+    image = Image.open(filename)
 
-asciiCanvas: "list" = [["" * width for _ in range(height)]]
-for i in range(height):
-    for j in range(width):
-        if image.getpixel((j, i)) == 0:
-            continue
-        # if image.getpixel((j, i)) >= 200:
-        # asciiCanvas[i][j] = "."
-        string_representation += "."
-        # J is not possibly unbound moron
-        # there's no boundary you don't know that because you have a retarded
-        # implementation of arrays python
-    string_representation += "\n"
+    image = image.convert("1")
 
-with open("pixelinfo.txt", "w") as file:
-    for y in range(height):
-        for x in range(width):
-            pixel_value = image.getpixel((x, y))
-            file.write(f"Pixel at ({x}, {y}: {pixel_value})")
+    width, height = image.size
+
+    # width = width / 10
+    # height = height / 10
+
+    string_representation = ""
+    for i in range(int(height)):
+        for j in range(int(width)):
+            if image.getpixel((j, i)) == 0:
+                string_representation += " "
+            # if image.getpixel((j, i)) >= 200:
+            # asciiCanvas[i][j] = "."
+            else:
+                string_representation += "."
+            # J is not possibly unbound moron
+            # there's no boundary you don't know that because you have a retarded
+            # implementation of arrays python
+        string_representation += "\n"
+
+    return string_representation
+
+
+#
+# with open("pixelinfo.txt", "w") as file:
+#     for y in range(height):
+#         for x in range(width):
+#             pixel_value = image.getpixel((x, y))
+#             file.write(f"Pixel at ({x}, {y}: {pixel_value})")
 """
 stringrep = ""
 for row in asciiCanvas:
@@ -34,5 +43,37 @@ for row in asciiCanvas:
     stringrep += "\n"
 
 """
-with open("asciifile.txt", "w") as file:
-    file.write(string_representation)
+
+
+def imgtonp(filename):
+    im = Image.open(filename)
+    width, height = im.size
+    im = im.convert("1")
+    a = np.asarray(im)
+
+    return a, width, height
+
+
+def writeTofile(string_representation):
+    with open("asciifile.txt", "w") as file:
+        file.write(string_representation)
+
+
+def main():
+    print(stringrep("./butterfly.jpg"))
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+##########################################################################
+#### This is part of the code where i store random things for later ######
+##########################################################################
+
+narr, width, height = imgtonp("./lilly.png")
+    np.set_printoptions(threshold=np.inf)
+    print(narr)
+
+"""
