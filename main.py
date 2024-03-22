99aad2bb18d1ee58f0247b5401ef072b26f1c9c4
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-03-22 16:39:36 +0300

    another checkpoint

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
 from PIL import Image
 import numpy as np
+from rembg import remove
 
 
-def stringrep(filename):
-    image = Image.open(filename)
-    # image = image.reduce(3)
-    image = image.resize((600, 200))
+def stringrep(image):
+    image = image.resize((600, 400))
     image = image.convert("1")
 
     width, height = image.size
-
-    # width = width / 10
-    # height = height / 10
 
     string_representation = ""
     for i in range(int(height)):
         for j in range(int(width)):
             if image.getpixel((j, i)) == 0:
                 string_representation += " "
-            # if image.getpixel((j, i)) >= 200:
-            # asciiCanvas[i][j] = "."
             else:
-                string_representation += "X"
-            # J is not possibly unbound moron
-            # there's no boundary you don't know that because you have a retarded
-            # implementation of arrays python
+                string_representation += "#"
         string_representation += "\n"
 
     return string_representation
 
 
-#
-# with open("pixelinfo.txt", "w") as file:
-#     for y in range(height):
-#         for x in range(width):
-#             pixel_value = image.getpixel((x, y))
-#             file.write(f"Pixel at ({x}, {y}: {pixel_value})")
-"""
-stringrep = ""
-for row in asciiCanvas:
-    for item in row:
-        stringrep += str(item)
-    stringrep += "\n"
+# it's a numpy array though
+def npstringrep(image):
+    string_representation = ""
+    width, height = image.shape
+    for i in range(int(height)):
+        for j in range(int(width)):
+            if image[i][j] < 100:
+                string_representation += " "
+            else:
+                string_representation += "X"
+        string_representation += "\n"
 
-"""
+    return string_representation
+
+
+def remove_background(filename):
+    input = Image.open(filename)
+    input = remove(input)
+    return input
+
+
+def reduceImage(image, window_size=(2, 2)):
+    width, height = np.shape(image)
+    stride_y, stride_x = window_size
+    new_height = height // stride_y
+    new_width = width // stride_x
+
+    reduced_image = np.zeros((new_height, new_width), dtype=np.float32)
+
+    for y in range(0, height, stride_y):
+        for x in range(0, width, stride_x):
+            window = image[y : y + stride_y, x : x + stride_x]
+            average = np.mean(window)
+            reduced_image[y // stride_y, x // stride_x] = average
+
+    return reduced_image
 
 
 def imgtonp(filename):
@@ -61,7 +74,13 @@ def writeTofile(string_representation):
 
 
 def main():
-    print(stringrep("./gooners.jpg"))
+    # image = remove_background("./peter.jpg")
+    # print(stringrep(image))
+    image = Image.open("./peter.jpg")
+
+    image = image.convert("L")
+    npimage = np.array(image)
+    print(npstringrep(reduceImage(npimage, (5, 5))))
 
 
 if __name__ == "__main__":
